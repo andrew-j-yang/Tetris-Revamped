9943c2d9c6ba77c3dcf3495620e1aacb42ce2be3
Andrew Yang <114878323+andrew-j-yang@users.noreply.github.com>
2023-10-20 10:57:56 -0500

    Add files via upload

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ayang/Desktop/Northwestern 2021-2022 Archives/Northwestern Spring Quarter 2021-2022 Archives/Comp Sci 211/Tetris
+# Build directory: /Users/ayang/Desktop/Northwestern 2021-2022 Archives/Northwestern Spring Quarter 2021-2022 Archives/Comp Sci 211/Tetris/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test_model_test "model_test")
+set_tests_properties(Test_model_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/ayang/Desktop/Northwestern 2021-2022 Archives/Northwestern Spring Quarter 2021-2022 Archives/Comp Sci 211/Tetris/.cs211/cmake/211commands.cmake;90;add_test;/Users/ayang/Desktop/Northwestern 2021-2022 Archives/Northwestern Spring Quarter 2021-2022 Archives/Comp Sci 211/Tetris/CMakeLists.txt;21;add_test_program;/Users/ayang/Desktop/Northwestern 2021-2022 Archives/Northwestern Spring Quarter 2021-2022 Archives/Comp Sci 211/Tetris/CMakeLists.txt;0;")
+subdirs(".cs211/lib/catch")
+subdirs(".cs211/lib/ge211")
